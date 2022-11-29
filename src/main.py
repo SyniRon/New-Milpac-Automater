aee012183fdcf93404e1ffc0e814f80dee37840c
kynetical <66834451+kynetical@users.noreply.github.com>
2022-11-29 00:47:28 -0500

    large refactor using new fuction
    replaced basically every single direct\
    site interaction with one function

--- src/main.py
+++ src/main.py
@@ -20,6 +20,36 @@ driver.get("https://7cav.us/")
 print("Trying to Login")
 
 
+def ele_int(ele_type, ele, int_type, load=""):
+    """Click eleement given.
+
+    Args:
+        ele_type (string): type of element to be clicked
+        ele (string): element to be clicked
+        int_type (int): type of interaction
+        1 = click
+        2 = send keys
+        3 = select from dropdown list
+        load (string): payload if needed
+    """
+    try:
+        int_it = WebDriverWait(driver, timeout=10).until(
+            lambda d: d.find_element(ele_type, ele)
+        )
+        if int_type == 1:
+            int_it.click()
+        elif int_type == 2:
+            int_it.clear()
+            int_it.send_keys(load)
+        elif int_type == 3:
+            select = Select(int_it)
+            select.select_by_visible_text(load)
+        else:
+            print("Invalid interaction type sent")
+    except TimeoutError:
+        print("Element not found in time")
+
+
 def login_attempt(driver):
     """Automatic Site Login.
 
@@ -38,17 +68,10 @@ def login_attempt(driver):
         print("Retrying Login")
     else:
         confirm_login(driver)
-    WebDriverWait(driver, timeout=10).until(
-        ec.presence_of_element_located((By.XPATH, '//*[@id="username"]'))
-    )
-    email_field = driver.find_element("xpath", '//*[@id="username"]')
-    password_field = driver.find_element("xpath", '//*[@id="password"]')
-    login_button2 = driver.find_element("xpath", '//*[@id="kc-login"]')
-    email_field.clear()
-    email_field.send_keys(username)
-    password_field.clear()
-    password_field.send_keys(password)
-    login_button2.click()
+    # input username
+    ele_int("xpath", '//*[@id="username"]', 2, username)
+    ele_int("xpath", '//*[@id="password"]', 2, password)
+    ele_int("xpath", '//*[@id="kc-login"]', 1)
     try:
         driver.find_element("xpath", '//*[@id="kc-content-wrapper"]/div[1]')
     except NoSuchElementException:
@@ -65,25 +88,15 @@ def confirm_login(driver):
         driver (webdriver): chosen webdriver
     """
     print("Logging In")
-    loginbutton = driver.find_element(
-        "xpath",
-        '//*[@id="top"]/div[2]/div[2]/div[2]/div/nav/div/div[3]/div[1]/a[1]',
+    ele_int(
+        "xpath", '//*[@id="top"]/div[2]/div[2]/div[2]/div/nav/div/div[3]/div[1]/a[1]', 1
     )
-    loginbutton.click()
-    WebDriverWait(driver, timeout=10).until(
-        ec.element_to_be_clickable(
-            (
-                By.XPATH,
-                '//*[@id="js-XFUniqueId5"]/div/div[2]/div/div/div/div/dl/dd\
+    ele_int(
+        "xpath",
+        '//*[@id="js-XFUniqueId5"]/div/div[2]/div/div/div/div/dl/dd\
                     /ul/li/a',
-            )
-        )
+        1,
     )
-    key_cloak_button = driver.find_element(
-        "xpath",
-        '//*[@id="js-XFUniqueId5"]/div/div[2]/div/div/div/div/dl/dd/ul/li/a',
-    )
-    key_cloak_button.click()
 
 
 def two_fa(driver):
@@ -92,29 +105,24 @@ def two_fa(driver):
     Args:
         driver (webdriver): selected webdriver
     """
-    WebDriverWait(driver, timeout=10).until(
-        ec.presence_of_element_located((By.NAME, "code"))
-    )
-    two_fa_field = driver.find_element(By.NAME, "code")
+    # requests 2FA code from user and stores to variable
     two_fa_code = input("Enter 2FA Code:")
-    two_fa_field.clear()
-    two_fa_field.send_keys(two_fa_code)
-    two_fa_confirm = driver.find_element(
-        By.XPATH,
+    # enters 2FA code into box
+    ele_int("name", "code", 2, two_fa_code)
+    # clicks confirm button
+    ele_int(
+        "xpath",
         '//*[@id="top"]/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/form/div\
             /dl/dd/div/div[2]/button',
+        1,
     )
-    two_fa_confirm.click()
     try:
         driver.find_element("xpath", '//*[@id="js-XFUniqueId2"]/div/div[1]')
     except NoSuchElementException:
         print("2FA Confirmed")
     else:
         print("Incorrect 2FA Code")
-        close_two_fa_warning = driver.find_element(
-            "xpath", '//*[@id="js-XFUniqueId2"]/div/div[1]/a'
-        )
-        close_two_fa_warning.click()
+        ele_int("xpath", '//*[@id="js-XFUniqueId2"]/div/div[1]/a', 1)
         two_fa(driver)
 
 
@@ -124,21 +132,11 @@ def milpac_nav(driver):
     Args:
         driver (webdriver)): chosen webdriver
     """
-    WebDriverWait(driver, timeout=10).until(
-        ec.presence_of_element_located(
-            (
-                By.XPATH,
-                '//*[@id="top"]/div[3]/div[2]/div[2]/div/nav/div/div[2]\
-                    /div/ul/li[2]/div/a',
-            )
-        )
+    ele_int(
+        "xpath",
+        '//*[@id="top"]/div[3]/div[2]/div[2]/div/nav/div/div[2]/div/ul/li[2]/div/a',
+        1,
     )
-    milpac_link = driver.find_element(
-        By.XPATH,
-        '//*[@id="top"]/div[3]/div[2]/div[2]/div/nav/div/div[2]\
-            /div/ul/li[2]/div/a',
-    )
-    milpac_link.click()
 
 
 def milpac_create(driver):
@@ -147,21 +145,13 @@ def milpac_create(driver):
     Args:
         driver (webdriver): chosen webdriver
     """
-    WebDriverWait(driver, timeout=10).until(
-        ec.presence_of_element_located(
-            (
-                By.XPATH,
-                '//*[@id="top"]/div[3]/div[2]/div[5]/div/div/div[2]\
-                    /div[1]/div/div/a',
-            )
-        )
-    )
-    add_user_button = driver.find_element(
+    ele_int(
         "xpath",
         '//*[@id="top"]/div[3]/div[2]/div[5]/div/div/div[2]\
             /div[1]/div/div/a',
+        1,
     )
-    add_user_button.click()
+    # waits for create milpac page to load
     WebDriverWait(driver, timeout=10).until(
         ec.presence_of_element_located(
             (
@@ -171,51 +161,25 @@ def milpac_create(driver):
             )
         )
     )
-    milpac_username = input("Enter forum username:")
+    # requests current username, new username, real name, join date
+    milpac_username = input("Enter current forum username:")
     milpac_username_update = input("Update username to the following:")
     milpac_real_name = input("Enter Real Name:")
     milpac_join_date = input("Enter Join Date (YYYY-MM-DD):")
-    milpac_username_update_entry = milpac_data_entry(
-        driver, "username", milpac_username, "new_username"
-    )
-
-    milpac_username_update_checkbox = driver.find_element(
-        By.XPATH,
+    # uses above input to fill out new milpac creation fields
+    ele_int("name", "username", 2, milpac_username)
+    ele_int(
+        "xpath",
         '//*[@id="top"]/div[3]/div[2]/div[5]/div/div/div[2]/div[3]\
             /form/div/div/dl[2]/dd/ul/li/label/i',
+        1,
     )
-    milpac_username_update_checkbox.click()
-    milpac_username_update_entry.send_keys(milpac_username_update)
-    milpac_rank_select = milpac_data_entry(
-        driver, "real_name", milpac_real_name, "rank_id"
-    )
-    select = Select(milpac_rank_select)
-    select.select_by_visible_text("Recruit")
-    milpac_position_select = driver.find_element(By.NAME, "position_id")
-    select = Select(milpac_position_select)
-    select.select_by_visible_text("New Recruit")
-    milpac_promotion_date_entry = milpac_data_entry(
-        driver, "custom_fields[joinDate]", milpac_join_date, "custom_fields[promoDate]"
-    )
-
-    milpac_promotion_date_entry.send_keys(milpac_join_date)
-
-
-def milpac_data_entry(driver, arg1, arg2, arg3):
-    """Use args to find field and enter data.
-
-    Args:
-        driver (webdriver): chosen webdriver
-        arg1 (element name): name of element to find
-        arg2 (string): data to enter
-        arg3 (element name): name of element to return
-
-    Returns:
-        element: returns selenium element
-    """
-    milpac_entry = driver.find_element(By.NAME, arg1)
-    milpac_entry.send_keys(arg2)
-    return driver.find_element(By.NAME, arg3)
+    ele_int("name", "new_username", 2, milpac_username_update)
+    ele_int("name", "real_name", 2, milpac_real_name)
+    ele_int("name", "rank_id", 3, "Recruit")
+    ele_int("name", "position_id", 3, "New Recruit")
+    ele_int("name", "custom_fields[joinDate]", 2, milpac_join_date)
+    ele_int("name", "custom_fields[promoDate]", 2, milpac_join_date)
 
 
 def milpac_confirm(driver):
@@ -224,13 +188,18 @@ def milpac_confirm(driver):
     Args:
         driver (webdriver): selected webdriver
     """
-    milpac_confirm_button = driver.find_element(
-        By.XPATH,
-        '//*[@id="top"]/div[3]/div[2]/div[5]/div/div/\
+    # clicks the confirm button twice
+    # why? v0v but it wont work otherwise
+    # i think the calendar covers the button
+    for _ in range(2):
+        ele_int(
+            "xpath",
+            '//*[@id="top"]/div[3]/div[2]/div[5]/div/div/\
             div[2]/div[3]/form/dl/dd/div/div[2]/button',
-    )
-    milpac_confirm_button.click()
-    milpac_confirm_button.click()
+            1,
+        )
+    # waits to make sure no errors are returned, if they are repeats the whole process
+    # might be cool if we could figure out the specific issue instead of starting over
     try:
         WebDriverWait(driver, timeout=10).until(
             ec.presence_of_element_located(
@@ -242,36 +211,12 @@ def milpac_confirm(driver):
             )
         )
     except NoSuchElementException:
-        print("Invalid Data Entered")
-        milpac_confirm_error_button = driver.find_element(
-            By.XPATH, '//*[@id="js-XFUniqueId3"]/div/div[1]/a'
-        )
-        milpac_confirm_error_button.click()
+        print("Invalid Data Entered, Try Again")
+        ele_int("xpath", '//*[@id="js-XFUniqueId3"]/div/div[1]/a', 1)
         milpac_nav(driver)
     else:
         print("Milpac Successfully Created")
 
-
-try:
-    driver.find_element(
-        "xpath", '//*[@id="top"]/div[2]/div[2]/div[2]/div/nav/div/div[3]/div[1]/a[1]'
-    )
-except NoSuchElementException:
-    print("Already Logged In")
-else:
-    login_attempt(driver)
-
-try:
-    driver.find_element(
-        "xpath",
-        '//*[@id="top"]/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/form/div\
-            /div/dl[3]/dd/ul/li/label/span',
-    )
-except NoSuchElementException:
-    print("2FA Not Detected, skipping")
-else:
-    print("2FA Required")
-    two_fa(driver)
 
 try:
     driver.find_element(
