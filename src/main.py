698a43970ed1ecef1bed87b3b4f510daa0087aad
kynetical <66834451+kynetical@users.noreply.github.com>
2022-12-01 12:42:04 -0500

    initial gui buildout

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import getpass
 import os
 import time
 
+import customtkinter as ctk
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
@@ -15,16 +16,29 @@ from selenium.webdriver.support.wait import WebDriverWait
 # testing code to make browser stay open after runtime completion
 # TODO: remove testing code and make headless
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument("--headless")
+# chrome_options.add_experimental_option("detach", True)
 
 # set driver to use selected options
 driver = webdriver.Chrome(options=chrome_options)
 # testing code while browser isn't headless
 # TODO: remove irrelevant size code once browser is headless
 driver.set_window_size(1024, 768)
+# opens the browser to 7cav.us
 print("Opening Browser")
 driver.get("https://7cav.us/")
 print("Trying to Login")
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+root = ctk.CTk()
+root.geometry("500x350")
+
+
+def clear_frame():
+    for widget in root.winfo_children():
+        widget.destroy()
 
 
 def ele_int(ele_type, ele, int_type, load=""):
@@ -61,14 +75,14 @@ def ele_int(ele_type, ele, int_type, load=""):
         raise ValueError("Invalid int_type selection")
 
 
-def login_attempt(driver):
+def login_attempt(driver, username, password, frame):
     """Automatic Site Login.
 
     Args:
         driver (webdriver): selenium webdriver
     """
-    username = input("Enter 7cav.us Username(email):")
-    password = getpass.getpass(prompt="Enter 7cav.us Password:")
+    # username = input("Enter 7cav.us Username(email):")
+    # password = getpass.getpass(prompt="Enter 7cav.us Password:")
     try:
         driver.find_element(
             "xpath",
@@ -87,9 +101,12 @@ def login_attempt(driver):
         driver.find_element("xpath", '//*[@id="kc-content-wrapper"]/div[1]')
     except NoSuchElementException:
         print("Logged in")
+        confirm_login(driver)
     else:
         print("Invalid Username or Password Detected")
-        login_attempt(driver)
+        wrong_pword = ctk.CTkLabel(master=frame, text="Invalid Username or Password")
+        wrong_pword.pack(pady=12, padx=10)
+        wrong_pword.after(7000, wrong_pword.destroy)
 
 
 def confirm_login(driver):
@@ -108,20 +125,31 @@ def confirm_login(driver):
                     /ul/li/a',
         1,
     )
+    try:
+        driver.find_element(
+            "xpath",
+            '//*[@id="top"]/div[2]/div[2]/div[6]/div/div/div[2]/div[2]/form/div\
+                /div/dl[3]/dd/ul/li/label/span',
+        )
+    except NoSuchElementException:
+        print("2FA Not Detected, skipping")
+    else:
+        print("2FA Required")
+        twofa_screen()
 
 
-def two_fa(driver):
+def two_fa(driver, twofa_code):
     """Handle 2fa entry.
 
     Args:
         driver (webdriver): selected webdriver
     """
     # requests 2FA code from user and stores to variable
-    two_fa_code = input("Enter 2FA Code:")
+    # twofa_code = input("Enter 2FA Code:")
     # scroll to bottom of page to avoid click intercept
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     # enters 2FA code into box
-    ele_int("name", "code", 2, two_fa_code)
+    ele_int("name", "code", 2, twofa_code)
     # clicks confirm button
     ele_int(
         "xpath",
@@ -139,7 +167,9 @@ def two_fa(driver):
         print("2FA Confirmed")
 
 
-def milpac_create(driver):
+def milpac_create(
+    driver, milpac_username, milpac_username_update, milpac_real_name, milpac_join_date
+):
     """Create new milpac on combat roster from user entry.
 
     Args:
@@ -158,10 +188,10 @@ def milpac_create(driver):
         )
     )
     # requests current username, new username, real name, join date
-    milpac_username = input("Enter current forum username:")
-    milpac_username_update = input("Update username to the following:")
-    milpac_real_name = input("Enter Real Name:")
-    milpac_join_date = input("Enter Join Date (YYYY-MM-DD):")
+    # milpac_username = input("Enter current forum username:")
+    # milpac_username_update = input("Update username to the following:")
+    # milpac_real_name = input("Enter Real Name:")
+    # milpac_join_date = input("Enter Join Date (YYYY-MM-DD):")
     # uses above input to fill out new milpac creation fields
     ele_int("name", "username", 2, milpac_username)
     ele_int(
@@ -267,6 +297,123 @@ def milpac_puc_add(driver, created_milpac):
         time.sleep(0.2)
 
 
+def login_button_event(uname_entry, pword_entry, frame):
+    username = uname_entry.get()
+    password = pword_entry.get()
+    login_attempt(driver, username, password, frame)
+
+
+def login_screen():
+    clear_frame()
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+    label = ctk.CTkLabel(master=frame, text="New Milpac Creation Automation")
+    label.pack(pady=12, padx=10)
+
+    label2 = ctk.CTkLabel(
+        master=frame, text="Please Login using your 7cav.us credentials"
+    )
+    label2.pack(pady=12, padx=10)
+
+    uname_entry = ctk.CTkEntry(master=frame, placeholder_text="Username")
+    uname_entry.pack(pady=12, padx=10)
+
+    pword_entry = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+    pword_entry.pack(pady=12, padx=10)
+
+    button = ctk.CTkButton(
+        master=frame,
+        text="Login",
+        command=lambda: login_button_event(uname_entry, pword_entry, frame),
+    )
+    button.pack(pady=12, padx=10)
+
+
+def entry_screen():
+    clear_frame()
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+    label = ctk.CTkLabel(master=frame, text="New Milpac Creation Automation")
+    label.pack(pady=12, padx=10)
+
+    label2 = ctk.CTkLabel(
+        master=frame, text="Please enter required information to create new milpac"
+    )
+    label2.pack(pady=12, padx=10)
+
+    label3 = ctk.CTkLabel(master=frame, text="New Recruit's Current Forum Username")
+    label3.pack(pady=12, padx=10)
+
+    milpac_username_entry = ctk.CTkEntry(master=frame, placeholder_text="Old Username")
+    milpac_username_entry.pack(pady=12, padx=10)
+
+    label4 = ctk.CTkLabel(master=frame, text="New Recruit's New Forum Username")
+    label4.pack(pady=12, padx=10)
+
+    milpac_username_update_entry = ctk.CTkEntry(
+        master=frame, placeholder_text="New Username"
+    )
+    milpac_username_update_entry.pack(pady=12, padx=10)
+
+    label5 = ctk.CTkLabel(master=frame, text="New Recruit's Real Name")
+    label5.pack(pady=12, padx=10)
+
+    milpac_real_name_entry = ctk.CTkEntry(master=frame, placeholder_text="New Username")
+    milpac_real_name_entry.pack(pady=12, padx=10)
+
+    label6 = ctk.CTkLabel(master=frame, text="Join Date in format YYYY-MM-DD")
+    label6.pack(pady=12, padx=10)
+
+    milpac_join_date_entry = ctk.CTkEntry(master=frame, placeholder_text="YYYY-MM-DD")
+    milpac_join_date_entry.pack(pady=12, padx=10)
+
+    button = ctk.CTkButton(master=frame, text="Submit", command=milpac_button_event)
+    button.pack(pady=12, padx=10)
+
+
+def twofa_screen():
+    clear_frame()
+    frame = ctk.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+    label = ctk.CTkLabel(master=frame, text="Two Factor Login Authentication")
+    label.pack(pady=12, padx=10)
+
+    label2 = ctk.CTkLabel(master=frame, text="Please enter your Two Factor Code")
+    label2.pack(pady=12, padx=10)
+
+    twofa_entry = ctk.CTkEntry(master=frame, placeholder_text="Old Username")
+    twofa_entry.pack(pady=12, padx=10)
+
+    button = ctk.CTkButton(master=frame, text="Submit", command=twofa_button_event)
+    button.pack(pady=12, padx=10)
+
+
+def milpac_button_event():
+    milpac_username = milpac_username_entry.get()
+    milpac_username_update = milpac_username_update_entry.get()
+    milpac_real_name = milpac_real_name_entry.get()
+    milpac_join_date = milpac_join_date_entry.get()
+    milpac_create(
+        driver,
+        milpac_username,
+        milpac_username_update,
+        milpac_real_name,
+        milpac_join_date,
+    )
+
+
+def twofa_button_event():
+    twofa_code = twofa_entry.get()
+    two_fa(driver, twofa_code)
+
+
+login_screen()
+root.mainloop()
+
+"""
 try:
     driver.find_element("xpath", '//*[@id="XF"]/body/div[2]/ul/li/div/div[2]/a[1]')
 except NoSuchElementException:
@@ -297,6 +444,7 @@ else:
     two_fa(driver)
 
 
-milpac_create(driver)
-created_milpac = milpac_confirm(driver)
-milpac_puc_add(driver, created_milpac)
+# milpac_create(driver)
+# created_milpac = milpac_confirm(driver)
+# milpac_puc_add(driver, created_milpac)
+"""
